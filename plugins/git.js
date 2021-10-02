17688a8c0cdcb918f4a21bea9e267d519864c8a0
jinusir <91747327+jinuttan@users.noreply.github.com>
2021-10-02 16:22:25 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/BVLUrXJ.jpeg";
+  r_text[0] = "https://i.ibb.co/TcMSXvf/Pics-Art-10-02-02-56.jpg";
     
     
     var i = Math.floor(1*Math.random())
